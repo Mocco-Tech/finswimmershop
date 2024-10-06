a3189f8f720692832b5721ebe86d0d9eb5b56cf5
David Lyashuk <info@moccotech.com>
2024-10-06 14:19:51 +0100

    Improved  XML file generation

--- src/app/collections/[handle]/feed.xml/route.ts
+++ src/app/collections/[handle]/feed.xml/route.ts
@@ -9,6 +9,8 @@ export async function GET(
   const collection: CollectionType = await getCollection(params.handle);
   const productsArr = collection.data.collectionByHandle.products.edges;
 
+  console.log(productsArr[0].node.images.edges);
+
   const feedBuilder = new FeedBuilder();
 
   feedBuilder.withTitle(collection.data.collectionByHandle.title);
@@ -17,12 +19,18 @@ export async function GET(
 
   if (productsArr) {
     productsArr.map((product) => {
+      const additionalImages =
+        product.node.images.edges.length > 1
+          ? product.node.images.edges.map((image) => image.node.url)
+          : [];
+
       feedBuilder.withProduct({
         id: product.node.id.replace("gid://shopify/Product/", ""),
         title: product.node.title,
         description: product.node.description.substring(0, 180),
         link: `http://www.finswimmershop.com/product/${product.node.handle}`,
         imageLink: product.node.images.edges[0].node.url,
+        additionalImageLinks: additionalImages,
         availability: "in_stock",
         price: {
           currency: product.node.priceRange.minVariantPrice.currencyCode,
